1f41283dc7efc065394b588c95898fa239383f72
Atta M Laghari <141480200+Atta-M-Laghari@users.noreply.github.com>
2024-09-27 19:11:55 +0500

    Add files via upload
    The Largest E-Commerce Cosmetic-Jewelry-Website-main Website

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ecombase";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Checking For Connection With Database 
+
+// if ($conn == true) {
+//     echo "Connection Successfull With Database";
+// } else {
+//     echo "Connection Failed With Database";
+// }
+?>
+
